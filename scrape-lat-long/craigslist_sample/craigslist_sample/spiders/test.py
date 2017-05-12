cc1d23366f398888bb54b195d19c72de95563ea3
Shubha Raghvendra <shubharaghvendra@gmail.com>
2017-05-11 20:38:20 -0700

    Added more comments for AA

--- scrape-lat-long/craigslist_sample/craigslist_sample/spiders/test.py
+++ scrape-lat-long/craigslist_sample/craigslist_sample/spiders/test.py
@@ -7,12 +7,17 @@ class MySpider(scrapy.Spider):
     name = "craig"
     #allowed_domains = ["craigslist.org"]
     def start_requests(self):
+        ## Create huge map of all of the lat / longs from our image set 
+        ## Use that to create a list of URLs with the pattern below
+        ## Change this to a for URL in URLs, yield ... [calls parse at the end] 
         url = "http://data.fcc.gov/api/block/find?format=json&latitude=37.714439&longitude=-122.214456&showall=true"
         yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
         for quote in response.css('div.quote'):
             print quote 
+        # This is initialized with an "items" object 
+        
         # hxs = HtmlXPathSelector(response)
         # titles = hxs.xpath("//span[@class='pl']")
         # print titles 
@@ -27,4 +32,3 @@ class MySpider(scrapy.Spider):
 
     # https://doc.scrapy.org/en/latest/topics/debug.html 
     # using the type of URL given above 
-    
