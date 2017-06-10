dc15df21748368d7e210e39e92198ae7e025c1ee
Helen Fang <hfang9@stanford.edu>
2017-06-10 20:35:20 +0000

    Weights are all down one line

--- conv_visualize_weights.py
+++ conv_visualize_weights.py
@@ -1,5 +1,13 @@
 from math import sqrt, ceil
 import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import torchvision
+import torchvision.transforms as T
+from torchvision.datasets import ImageFolder
 
 def visualize_grid(Xs, ubound=255.0, padding=1):
     """
@@ -35,8 +43,15 @@ def visualize_grid(Xs, ubound=255.0, padding=1):
     # grid = ubound * (grid - grid_min) / (grid_max - grid_min)
     return grid
 
+modelpath='layer4-1000imgtest.pytorch'
 
-grid = visualize_grid(model.params['W1'].transpose(0, 2, 3, 1)) #CHANGE TO CONVLAYER WE WANT
+model = torch.load(modelpath)
+print(model.layer4)
+print '\n'
+conv1 = list(model.layer4[1].conv1.parameters())[0]
+print('conv1 size')
+conv1_array = ((conv1.data).cpu().numpy())
+grid = visualize_grid(conv1_array) #CHANGE TO CONVLAYER WE WANT
 plt.imshow(grid.astype('uint8'))
 plt.axis('off')
 plt.gcf().set_size_inches(5, 5)
