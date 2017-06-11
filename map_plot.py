84fa28f5e51316e0aecfb1e3b49b4c5913ab9774
Helen Fang <hfang9@stanford.edu>
2017-06-11 05:25:26 +0000

    Fixed up the code

--- map_plot.py
+++ map_plot.py
@@ -5,8 +5,8 @@ import numpy as np
 print "starting"
 map = Basemap(projection='merc', lat_0 = 57, lon_0 = -135,
     resolution = 'h', area_thresh = 0.1,
-    llcrnrlon=-122.5, llcrnrlat=37.5,
-    urcrnrlon=-122.0, urcrnrlat=38.0)
+    llcrnrlon=-122.4, llcrnrlat=37.7,
+    urcrnrlon=-122.15, urcrnrlat=37.9)
  
 map.drawcoastlines()
 map.drawmapboundary(fill_color='#ADD8E6')
@@ -15,8 +15,9 @@ map.fillcontinents(color = '#C0C0C0')
 map.drawmapboundary()
 
 # Plot ground truth mappings
-fname = "misclassified.txt"
+fname = "misclassified-50k.txt"
 
+correct = []
 corr_low = []
 corr_med = []
 corr_high = []
@@ -25,15 +26,35 @@ with open(fname) as f:
 	for currline in f:
 		line = currline.strip().split()
 		if line[1] == line[2]:
-			if line[1].strip() == '0':
+			correct.append(line)
+			if line[1].strip() == '1':
 				corr_low.append(line)
-			elif line[1].strip() == '1':
+			elif line[1].strip() == '2':
 				corr_med.append(line)
 			else:
 				corr_high.append(line)
 		else:
 			incorrect.append(line)		
 
+##### THIS SECTION FOR DISPLAYING CORRECT/INCOREECT PRED
+lat_corr = []
+long_corr = []
+for corr in incorrect:
+        coord = corr[0].split('_')
+        lat_corr.append(float(coord[1].split('/')[-1]))
+        long_corr.append(float(coord[2]))
+x, y = map(long_corr, lat_corr)
+map.scatter(x, y,c='#ff0000', marker='o', s=1,zorder=10)
+lat_corr = []
+long_corr = []
+for corr in correct:
+        coord = corr[0].split('_')
+        lat_corr.append(float(coord[1].split('/')[-1]))
+        long_corr.append(float(coord[2]))
+x, y = map(long_corr, lat_corr)
+map.scatter(x, y, c='#9370db',marker='o', s=1,zorder=10)
+'''
+##### THIS SECTION FOR DISPLAYING TRUE SECTION #######
 lat_corr = []
 long_corr = []
 for corr in corr_low:
@@ -41,7 +62,7 @@ for corr in corr_low:
 	lat_corr.append(float(coord[1].split('/')[-1]))
 	long_corr.append(float(coord[2]))
 x, y = map(long_corr, lat_corr)
-map.plot(x, y, 'bo', markersize=5)
+map.scatter(x, y,c='#ee82ee', marker='o', s=1,zorder=10)
 lat_corr = []
 long_corr = []
 for corr in corr_med:
@@ -49,7 +70,7 @@ for corr in corr_med:
         lat_corr.append(float(coord[1].split('/')[-1]))
         long_corr.append(float(coord[2]))
 x, y = map(long_corr, lat_corr)
-map.plot(x, y, 'go', markersize=5)
+map.scatter(x, y, c='#ba55d3',marker='o', s=1,zorder=10)
 lat_corr = []
 long_corr = []
 for corr in corr_high:
@@ -57,6 +78,7 @@ for corr in corr_high:
         lat_corr.append(float(coord[1].split('/')[-1]))
         long_corr.append(float(coord[2]))
 x, y = map(long_corr, lat_corr)
-map.plot(x, y, 'yo', markersize=5)
+map.scatter(x, y,c='#800080', marker='o', s=1,zorder=10)
+'''
 print "plotted markers?"
 plt.show()
